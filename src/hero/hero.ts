2f0df75c749109caba66d89ae0e8857a6647e08f
sky4564 <qw486512@gmail.com>
2025-01-23 10:55:25 +0900

    3

---
+++ src/hero/hero.ts
@@ -0,0 +1,13 @@
+const hero = 'superman'
+
+function help(x: string): boolean {
+  console.log(`help me  ${hero} !!!`)
+  return true
+}
+
+help(hero)
+help(hero)
+help(hero)
+
+
+
---
+++ src/tester/test.ts
@@ -0,0 +1,8 @@
+const tester = 'tom'
+
+function test(x: string): boolean {
+    console.log('test')
+    return true
+}
+
+test(tester)
